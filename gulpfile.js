f8aa68c0cad09c4cdc7e200227f3b31590c669da
Skyz03 <aakibdiscoo@gmail.com>
2022-07-31 17:45:44 +0545

    Working gulp file without imgminification

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+// const imagemin = require("gulp-imagemin");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest("dist/css", { sourcemaps: "." }));
+}
+
+// Optimize Images
+// gulp.task("imageMin", () =>
+//   gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("dist/images"))
+// );
+
+// function imageMin() {
+//   return src("app/img/*")
+//     .pipe(imagemin())
+//     .pipe(dest("dist/img", { sourcemaps: "." }));
+// }
+
+// JavaScript Task
+function jsTask() {
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist/js", { sourcemaps: "." }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browsersyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+// Default Gulp task
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
